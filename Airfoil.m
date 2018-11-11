b6cda3861cd8172dc4f03ad126dd3cd57d2459ad
Jacob Regenstein <jacobregenstein@gmail.com>
2018-11-10 22:54:19 -0500

    implemented get_CL(a),get_CD(a), helper functions

--- Airfoil.m
+++ Airfoil.m
@@ -5,16 +5,48 @@
 %converge
 classdef Airfoil
     properties
-        type %type of airfoil, to start with it will just be a NACA 4-digit
+        type; %type of airfoil, to start with it will just be a NACA 4-digit
              %but we will want to include the whole UIUC library of
              %airfoils 1st
+        data;%array that stores the airfoil data. 
+        %add iteration number as a constant
     end
     methods
         %make a constructor that takes in an airfoil type and sweeps over
         %range of alphas
-        
+        function obj = Airfoil(type)
+            obj.type = type;
+            obj.data = xfoil(obj.type,-5:0.5:30,200000,0,Plane.XFOIL_ITERATIONS);
+        end
         %make functions to lookup Cl and Cd as a function of alpha, nice to
         %haves would be functions that give the max Cl, max Cl/Cd, and
         %functions that give the respective alphas for both of those points
+        function cl = get_CL(obj,alpha)
+            %gets the index associated with the alpha. Could do this in one
+            %line but I think it's more readable this way
+            i = obj.get_index(alpha);
+            cl = obj.data.CL(i);
+        end
+        function cd = get_CD(obj,alpha)
+            i = obj.get_index(alpha);
+            cd = obj.data.CD(i);
+        end
+        %gets the index corresponding to the given AoA in the data array.
+        %To start with it will just give the index, but eventually we will
+        %want to set it to find the nearest alpha values and interpolate
+        %between them
+        function i = get_index(obj, alpha)
+            i = 1;
+            %if there is an array index out of bounds issue with the
+            %following line, it's most likely because the client code
+            %requested an AoA that was outside of the tested range. This
+            %isn't a good way to do this, the correct way is to learn how
+            %to throw and catch errors in matlab, but that's a task for
+            %another day. Hopefully once the code is able to interpolate,
+            %we'll also be able to get it to extrapolate
+            while(obj.data.alpha(i)<alpha)
+                i = i + 1;
+            end
+        end
     end
 end
